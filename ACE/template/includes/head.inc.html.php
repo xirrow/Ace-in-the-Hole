eabd6ced42e851f157c85497358753f4d6a6ad38
xirrow <45326541+xirrow@users.noreply.github.com>
2019-02-12 21:50:49 -0800

    Add files via upload

---
+++ ACE/template/includes/head.inc.html.php
@@ -0,0 +1,16 @@
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php page_Title(); ?></title>
+
+    <style><?php include 'css/styles.css'; ?>"</style>
+    <style><?php include 'css/grid-spec.css'; ?></style>
+    <style><?php include 'css/slideshow.css'; ?></style>
+    
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
---
+++ ACE/template/includes/nav.inc.html.php
@@ -0,0 +1,18 @@
+<nav>
+    <ul class="topnav">
+        <div class="container">
+            <a href="<?php echo 'index.php'; ?>"><img id="nav-logo" src="<?php echo 'images/logo/logo-red-01.png'; ?>"></a>
+            <div class="topnav-right">
+                <li><a href="<?php echo 'index.php'; ?>">HOME</a></li>
+                <li><a href="<?php echo '#about'; ?>">ABOUT</a></li>
+                <li><a href="<?php echo '#'; ?>">EVENT INFO</a></li>
+                <li><a href="<?php echo '#'; ?>">FAQ</a></li>
+                <li><a href="<?php echo '#'; ?>">CONTACT</a></li>
+                <li><a href="<?php echo '#'; ?>">REGISTER</a></li>
+                <li class="icon">
+                    <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
+                </li>
+            </div>
+        </div>
+    </ul>
+</nav>
